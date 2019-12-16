66c3818b6a39ef563af44bccbc96de0813737995
Rob Maas <rob@progob.nl>
2019-12-16 20:32:11 +0100

    Update vm.tf

--- lab-examples/lab01/vm.tf
+++ lab-examples/lab01/vm.tf
@@ -48,7 +48,7 @@ resource "azurerm_network_security_group" "nsg" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "22"
-        source_address_prefix      = "185.46.232.0/22"
+        source_address_prefix      = "188.207.248.162"
         destination_address_prefix = "*"
     }
 }
